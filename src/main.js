f0ef764f37f18a802c685ac6d81e2027965d7584
James Thaumiel <starchturrets@protonmail.com>
2019-08-17 18:38:30 +0800

    Style changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import App from './App.svelte';
 
-const arrayOfItems = [];
+// const arrayOfItems = [
+//   {
+//     done: false,
+//     string: 'Something to do',
+//   },
+// ];
 const app = new App({
   target: document.body,
   props: {
     name: 'world',
-    arr: arrayOfItems,
+    // arr: arrayOfItems,
   },
 });
 
